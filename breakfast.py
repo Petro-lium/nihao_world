2e337e18a4ec66ebf0a0136a7893c8865ee7413f
Sergei Strygin <sew1982@gmail.com>
2020-03-16 22:04:38 +0300

    new change

---
+++ breakfast.py
@@ -0,0 +1,26 @@
+#Практикум яндекс
+#Готовим завтрак. Можно приготовить хлопья с молоком (молоко, хлопья),
+#можно яичницу (яйца),
+#а можно омлет (молоко и яйца). Есть и более бюджетные варианты.
+
+milk = True
+cereals = False
+eggs = True
+
+if milk or cereals or eggs:
+    if eggs:
+        if milk:
+            breakfast = '- омлет'
+        else:
+            breakfast = '- яичница'
+    else:
+        breakfast = '- хлопья с молоком'
+else:
+    if milk:
+        breakfast = '- стакан молока'
+    elif cereals:
+        breakfast = 'можно погрызть сухих хлопьев'
+    else:
+        breakfast = 'ничего не будет: разгрузочный день'
+
+print('Сегодня на завтрак', breakfast)
